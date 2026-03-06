c7bac27d056a801f2eb88aab2f2b10c8a0b1930d
Thangnvhe <thangshk08@gmail.com>
2026-03-06 13:46:33 +0700

    Create an initial project

---
+++ Lesson01_API/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapControllers();
+app.Run();
